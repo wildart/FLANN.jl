16dd49cfd3192a7b33146913661202cf0640abe5
Art <wildart@gmail.com>
2017-05-15 23:13:20 -0400

    added windows binary

--- deps/build.jl
+++ deps/build.jl
@@ -11,7 +11,13 @@ libflann = library_dependency("libflann",
 provides(Sources,
          URI("https://github.com/mariusmuja/flann/archive/$(flann_version).tar.gz"),
          libflann,
-         unpacked_dir="flann-$(flann_version)")
+         unpacked_dir="flann-$(flann_version)",
+         os = :Unix)
+provides(Binaries,
+         URI("https://github.com/wildart/FLANN.jl/releases/download/v0.0.5/libflann-windows-amd64-$(flann_version)-julia-0.5.1.tar.gz"),
+         libflann,
+         unpacked_dir=BinDeps.libdir(libflann) ,
+         os = :Windows)
 
 flannusrdir = BinDeps.usrdir(libflann)
 flannsrcdir = joinpath(BinDeps.srcdir(libflann),"flann-$(flann_version)")
